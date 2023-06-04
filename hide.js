19a665fdd818cc84db482aafd42bc7648954ddcb
pmaxy <84446514+pmaxy@users.noreply.github.com>
2023-06-03 23:02:02 -0600

    add files

---
+++ hide.js
@@ -0,0 +1,30 @@
+const heading1=document.querySelector('.intro');
+const headline=document.querySelector('.headline');
+const workMore=document.querySelector('.project_description');
+const para=document.querySelector('.');
+const drop=[
+    {
+        description1:'I worked on numerous initiatives as a volunteer in the Kisumu County government`s GIS division....',
+        description: 'I worked on numerous initiatives as a volunteer in the Kisumu County government`s GIS division. I mapped the county`s gas stations and gathered information on the Sondu and Katito markets for land-use planning. In addition, I reviewed the GIS draft evaluation roll for Kisumu County and took part in labor market planning. Additionally, I aided in the analysis of climatic and weather changes and worked on geographical analytic projects with Transparency International. In addition, I helped UN-Habitat map socioeconomic information in Kisumu city`s informal settlements and collected data and made maps of the handwashing stations that are already present there for COVID-19 mitigation.'
+    }
+    ,
+    {
+        description1:'I have successfully created geodatabases for Kobo`s humanitarian server and desktop applications....',
+        description:'I have successfully created geodatabases for Kobo`s humanitarian server and desktop applications as a seasoned GIS professional. The SEACAP initiative, which focuses on access to sustainable energy and climate action, also relies heavily on the analysis and presentation of spatial data, and I have played a critical part in both of these endeavors. I have been able to contribute to these initiatives successfully according to my knowledge of geodatabase design and spatial data analysis, demonstrating my aptitude for using GIS technology for data management and visualization in support of important projects.'
+    }
+    ,
+    {
+        description1:'I have successfully created geodatabases for Kobo`s humanitarian server and desktop applications',
+        description:'I have successfully created geodatabases for Kobo`s humanitarian server and desktop applications as a seasoned GIS professional. The SEACAP initiative, which focuses on access to sustainable energy and climate action, also relies heavily on the analysis and presentation of spatial data, and I have played a critical part in both of these endeavors. I have been able to contribute to these initiatives successfully according to my knowledge of geodatabase design and spatial data analysis, demonstrating my aptitude for using GIS technology for data management and visualization in support of important projects.'
+    }
+    ,
+    {
+        description1:'I have successfully created geodatabases for Kobo`s humanitarian server and desktop applications',
+        description:'I have successfully created geodatabases for Kobo`s humanitarian server and desktop applications as a seasoned GIS professional. The SEACAP initiative, which focuses on access to sustainable energy and climate action, also relies heavily on the analysis and presentation of spatial data, and I have played a critical part in both of these endeavors. I have been able to contribute to these initiatives successfully according to my knowledge of geodatabase design and spatial data analysis, demonstrating my aptitude for using GIS technology for data management and visualization in support of important projects.'
+    }
+]
+function introonclick(){
+    heading1.innerHTML="I’m a software developer! I can helpyou build a product , feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, don’t hestiate to contact me. I am also Highly skilled in GIS professional with a Bachelor of Science degree in Geospatial Information Science with IT. Proven experience in spatial analysis, data collection, and GIS mapping. Seeking challenging opportunities to contribute expertise in GIS and remote sensing to support organizations in making informed decisions and solving complex problems."
+
+}
+
