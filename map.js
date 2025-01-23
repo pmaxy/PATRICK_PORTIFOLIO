c5e52b9ffc265b698f7f2797bbc2cc44bcc5141e
Wanjiru <patrick.wanjiru1@equitybank.co.ke>
2025-01-23 17:56:49 +1200

    updated my portifolio

--- map.js
+++ map.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.505, -0.09], 13); // Set initial coordinates and zoom level
+var map = L.map('map').setView([36.9, -1.15], 13); // Set initial coordinates and zoom level
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
@@ -6,5 +6,5 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 // Add a marker to the map
-L.marker([51.5, -0.09]).addTo(map)
+L.marker([36.9, -1.15]).addTo(map)
 .bindPopup('A sample marker.');
--- popup2.js
+++ popup2.js
@@ -1,59 +1,86 @@
 const projectArr = [
   {
     id: 0,
-    title: 'Kisumu County Government',
-    details: ['vOLUNTEER ', 'GIS Analyst', 2019-2020],
-    image: 'img/tonic1.svg',
-    desktopImage: 'img/tonic1.svg',
-    description: 'I worked on numerous initiatives as a volunteer in the Kisumu County government`s GIS division. I mapped the county`s gas stations and gathered information on the Sondu and Katito markets for land-use planning. In addition, I reviewed the GIS draft evaluation roll for Kisumu County and took part in labor market planning. Additionally, I aided in the analysis of climatic and weather changes and worked on geographical analytic projects with Transparency International. In addition, I helped UN-Habitat map socioeconomic information in Kisumu city`s informal settlements and collected data and made maps of the handwashing stations that are already present there for COVID-19 mitigation.',
-    technologies: ['html', 'css', 'javaScript'],
+    title: 'Equity Bank Limited',
+    details: ['EMPLOYMENT', 'Devops Engineer', "2024-Present"],
+    image: 'img/equitybank.jpg',
+    desktopImage: 'img/equitybank.jpg',
+    description: 'Using Azure DevOps, I created and managed CI/CD pipelines that automated application builds, testing, and deployments. Developed scalable microservices and APIs in Java, allowing for easy integration of various data sources.Containerized apps were deployed using Docker and Kubernetes to ensure high availability and scalability across several environments.Infrastructure as Code (IaC) was implemented using Terraform, which streamlined resource provisioning. Pipelines are secured with vulnerability screening and access controls to ensure compliance with corporate requirements.',
+    technologies: ['Azure Devops', 'Docker', 'Kubernetes'],
     live: 'https://pmaxy.github.io/setup-and-mobile-first/',
     source: 'https://github.com/pmaxy/setup-and-mobile-first',
   },
   {
     id: 1,
-    title: 'Uhai Lake Forum ',
-    details: ['EMPLOYMENT', 'ASS SPATIAL ANALYST', 2021],
-    image: 'img/multistories1.svg',
-    desktopImage: 'img/multistories1.svg',
-    description: 'I have successfully created geodatabases for Kobo`s humanitarian server and desktop applications as a seasoned GIS professional. The SEACAP initiative, which focuses on access to sustainable energy and climate action, also relies heavily on the analysis and presentation of spatial data, and I have played a critical part in both of these endeavors. I have been able to contribute to these initiatives successfully according to my knowledge of geodatabase design and spatial data analysis, demonstrating my aptitude for using GIS technology for data management and visualization in support of important projects.',
-    technologies: ['html', 'css', 'javaScript'],
+    title: 'E&M Technologies',
+    details: ['EMPLOYMENT', 'Software Developer', 2023-2024],
+    image: 'img/em.webp',
+    desktopImage: 'img/em.webp',
+    description: 'Developed strong backend systems utilizing Java and Spring Boot to support a wide range of business applications.Created data-driven workflows to manage complicated processing tasks, using relational databases and cloud platforms.Collaboration with cross-functional teams improved system performance and scalability.',
+    technologies: ['Angular', 'Spring Boot', 'React'],
     live: 'https://pmaxy.github.io/setup-and-mobile-first/',
     source: 'https://github.com/pmaxy/setup-and-mobile-first',
   },
   {
     id: 2,
-    title: 'GEOPLAN Consultant',
-    details: ['EMPLOYMENT', 'Data collection Officer', 2021],
-    image: 'img/tonic2.svg',
-    desktopImage: 'img/tonic2.svg',
-    description: 'I had the chance to contribute to the Oyugis Local Land Use Development Plan (LPLUDP) as a skilled data analyst and gatherer. Editing cadastral files and carrying out data collection tasks were among my duties. I was a key contributor to the creation and execution of the land use plan by guaranteeing accurate and current information. I helped the project get off the ground successfully by paying close attention to the little things and being dedicated to excellence.',
-    technologies: ['html', 'css', 'javaScript'],
-    live: 'https://pmaxy.github.io/setup-and-mobile-first/',
-    source: 'https://github.com/pmaxy/setup-and-mobile-first',
-  },
-  {
-    id: 3,
     title: 'GOOGLE PLUS CODE',
     details: ['EMPLOYMENT', 'Supervisopr', 2022],
-    image: 'img/multistories2.svg',
-    desktopImage: 'img/multistories2.svg',
+    image: 'img/pluscode.png',
+    desktopImage: 'img/pluscode.png',
     description: 'From November 2021 to December 2022, I had the duty of educating enumerators and managing the digital addressing procedures for Kisumu County households as a Google Plus Code Trainer and Supervisor. Through this position, I was able to make sure that the addressing procedures were followed precisely and effectively as well as that the training was thorough and effective. I significantly improved the county`s digital addressing system, which helped to make it more accessible and organized for the citizens.',
-    technologies: ['html', 'css', 'javaScript'],
+    technologies: ['Arcpy', 'Python', 'ArcPro'],
+    live: 'https://pmaxy.github.io/setup-and-mobile-first/',
+    source: 'https://github.com/pmaxy/setup-and-mobile-first',
+  }, 
+  {
+    id: 3,
+    title: 'Uhai Lake Forum ',
+    details: ['EMPLOYMENT', 'ASS SPATIAL ANALYST', 2021],
+    image: 'img/uhai.jpg',
+    desktopImage: 'img/uhai.jpg',
+    description: 'I have successfully created geodatabases for Kobo`s humanitarian server and desktop applications as a seasoned GIS professional. The SEACAP initiative, which focuses on access to sustainable energy and climate action, also relies heavily on the analysis and presentation of spatial data, and I have played a critical part in both of these endeavors. I have been able to contribute to these initiatives successfully according to my knowledge of geodatabase design and spatial data analysis, demonstrating my aptitude for using GIS technology for data management and visualization in support of important projects.',
+    technologies: ['Spss', 'ArcPro', 'Excel'],
     live: 'https://pmaxy.github.io/setup-and-mobile-first/',
     source: 'https://github.com/pmaxy/setup-and-mobile-first',
   },
+
+
   {
     id: 4,
     title: 'Cyglobe Consultant',
     details: ['EMPLOYMENT', 'Data collection Officer', 2023],
-    image: 'img/multistories2.svg',
-    desktopImage: 'img/multistories2.svg',
+    image: 'img/cyglobe.jpg',
+    desktopImage: 'img/cyglobe.jpg',
     description: 'I engaged on Makindu Town Planning in April 2023, where I gathered and analyzed data. As part of my job, I had to acquire pertinent data and analyze it to help with town planning. Through this project, I was able to put my data analytic talents to use and help Makindu Town plan in an efficient way.',
-    technologies: ['html', 'css', 'javaScript'],
+    technologies: ['Autocad', 'Archicad', 'Qgis'],
     live: 'https://pmaxy.github.io/setup-and-mobile-first/',
     source: 'https://github.com/pmaxy/setup-and-mobile-first',
   },
+  {
+    id: 5,
+    title: 'GEOPLAN Consultant',
+    details: ['EMPLOYMENT', 'Data collection Officer', 2021],
+    image: 'img/GeoPlan_Logo.png',
+    desktopImage: 'img/GeoPlan_Logo.png',
+    description: 'I had the chance to contribute to the Oyugis Local Land Use Development Plan (LPLUDP) as a skilled data analyst and gatherer. Editing cadastral files and carrying out data collection tasks were among my duties. I was a key contributor to the creation and execution of the land use plan by guaranteeing accurate and current information. I helped the project get off the ground successfully by paying close attention to the little things and being dedicated to excellence.',
+    technologies: ['Qgis', 'Arcmap', 'ArcPro'],
+    live: 'https://pmaxy.github.io/setup-and-mobile-first/',
+    source: 'https://github.com/pmaxy/setup-and-mobile-first',
+  },
+  {
+    id: 6,
+    title: 'Kisumu County Government',
+    details: ['vOLUNTEER ', 'GIS Analyst', 2019-2020],
+    image: 'img/Kisumu county beach.jpg',
+    desktopImage: 'img/Kisumu county beach.jpg',
+    description: 'I worked on numerous initiatives as a volunteer in the Kisumu County government`s GIS division. I mapped the county`s gas stations and gathered information on the Sondu and Katito markets for land-use planning. In addition, I reviewed the GIS draft evaluation roll for Kisumu County and took part in labor market planning. Additionally, I aided in the analysis of climatic and weather changes and worked on geographical analytic projects with Transparency International. In addition, I helped UN-Habitat map socioeconomic information in Kisumu city`s informal settlements and collected data and made maps of the handwashing stations that are already present there for COVID-19 mitigation.',
+    technologies: ['Qgis', 'Arcpy', 'Python'],
+    live: 'https://pmaxy.github.io/setup-and-mobile-first/',
+    source: 'https://github.com/pmaxy/setup-and-mobile-first',
+  },
+
+ 
+
 ];
 
 const popup = document.querySelector('.popup_menu');
@@ -108,8 +135,8 @@ btnClosePopup.addEventListener('click', () => {
 });
 
 var map = L.map('map',{
-  center:[-0.121606, 34.781211],
-  zoom:10,
+  center:[-1.155057636939555, 36.913954345335874],
+  zoom:18,
   layers: USGS_USImagery
 });
   
@@ -141,10 +168,10 @@ var USGS_USImagery = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/se
 
 
 // Add a marker to the map
-L.marker([51.5, -0.09]).addTo(map)
+L.marker([-1.155057636939555, 36.913954345335874]).addTo(map)
  .bindPopup('A sample marker.');
 
- var marker=L.marker([-0.121606, 34.781211]).addTo(map);
+ var marker=L.marker([-1.155057636939555, 36.913954345335874]).addTo(map);
  marker.bindPopup('<b>Patrick Wanjiru the Developer<b/>')
 
  
